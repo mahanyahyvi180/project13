88f08163b372ac3a4e734b7435c768aa3be6c96c
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-07-29 17:12:59 +0330

    Add files via upload

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             private int[,] grades;
+
+        public string CourseName { get; }
+
+        public GradeBook(string name, int[,] gradesArray)
+        {
+            CourseName = name;
+            grades = gradesArray;
+        }
+
+        public void DisplayMessage()
+        {
+            Console.WriteLine($"Welcome to the grade book for\n{CourseName}!\n");
+        }
+
+        public void processGrades()
+        {
+
+            OutpuGrades();
+
+            Console.WriteLine($"\nLowest grade in the grade book is{GetMinimum()}" + $"\nHighest grade in the grade book is {GetMaximum()}\n");
+
+            OutputBarChart();
+        }
+
+
+
+    }
+}
+}
